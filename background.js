e5627380eaf20df1110df079020a2fdba560560d
Ankit <ankitrajdihara123@gmail.com>
2025-07-15 03:06:27 +0530

    done

--- background.js
+++ background.js
@@ -1,1 +1,21 @@
-    
+chrome.runtime.onInstalled.addListener(async () => {
+  const { blockedSites = [] } = await chrome.storage.local.get("blockedSites");
+
+  const rules = blockedSites.map((site, index) => ({
+    id: index + 1,
+    priority: 1,
+    action: {
+      type: "redirect",
+      redirect: { extensionPath: "/focus.html" },
+    },
+    condition: {
+      urlFilter: site,
+      resourceTypes: ["main_frame"],
+    },
+  }));
+
+  await chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: Array.from({ length: 100 }, (_, i) => i + 1),
+    addRules: rules,
+  });
+});
--- popup.js
+++ popup.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const listContainer = document.querySelector(".blocked-container");
   const clearBtn = document.querySelector(".third-part button:nth-child(1)");
   addBtn.addEventListener("click", async () => {
-    const site = input.value.trim();
+    const url = input.value.trim();
+    const site = new URL(
+      url.includes("http") ? url : "https://" + url
+    ).hostname.replace("www.", "");
     if (!site) return;
     const { blockedSites = [] } = await chrome.storage.local.get(
       "blockedSites"
@@ -13,23 +16,22 @@ document.addEventListener("DOMContentLoaded", () => {
     await chrome.storage.local.set({ blockedSites });
     input.value = "";
     renderBlockedList();
-
-    clearBtn.addEventListener("click", async () => {
-      await chrome.storage.local.set({ blockedSites: [] });
-      renderBlockedList();
-    });
-
-    async function renderBlockedList() {
-      const { blockedSites = [] } = await chrome.storage.local.get(
-        "blockedSites"
-      );
-      listContainer.innerHTML = "";
-      blockedSites.forEach((site) => {
-        const item = document.createElement("h1");
-        item.textContent = site;
-        listContainer.appendChild(item);
-      });
-    }
+  });
+  clearBtn.addEventListener("click", async () => {
+    await chrome.storage.local.set({ blockedSites: [] });
     renderBlockedList();
   });
+
+  async function renderBlockedList() {
+    const { blockedSites = [] } = await chrome.storage.local.get(
+      "blockedSites"
+    );
+    listContainer.innerHTML = "";
+    blockedSites.forEach((site) => {
+      const item = document.createElement("h1");
+      item.textContent = site;
+      listContainer.appendChild(item);
+    });
+  }
+  renderBlockedList();
 });
