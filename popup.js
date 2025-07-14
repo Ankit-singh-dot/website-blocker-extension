9e496ca18f1409f33d44684c8e582e1bfe0c08c5
Ankit <ankitrajdihara123@gmail.com>
2025-07-15 01:44:39 +0530

    appearing_added_skeletron_and_other_functionality

---
+++ popup.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector("input");
+  const addBtn = document.querySelector(".first-block button");
+  const listContainer = document.querySelector(".blocked-container");
+  const clearBtn = document.querySelector(".third-part button:nth-child(1)");
+  addBtn.addEventListener("click", async () => {
+    const site = input.value.trim();
+    if (!site) return;
+    const { blockedSites = [] } = await chrome.storage.local.get(
+      "blockedSites"
+    );
+    blockedSites.push(site);
+    await chrome.storage.local.set({ blockedSites });
+    input.value = "";
+    renderBlockedList();
+
+    clearBtn.addEventListener("click", async () => {
+      await chrome.storage.local.set({ blockedSites: [] });
+      renderBlockedList();
+    });
+
+    async function renderBlockedList() {
+      const { blockedSites = [] } = await chrome.storage.local.get(
+        "blockedSites"
+      );
+      listContainer.innerHTML = "";
+      blockedSites.forEach((site) => {
+        const item = document.createElement("h1");
+        item.textContent = site;
+        listContainer.appendChild(item);
+      });
+    }
+    renderBlockedList();
+  });
+});
